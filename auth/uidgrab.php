bc30ac44338d7f9d857bd663899fffd2d3e822d6
EffeX <48663440+effex1337@users.noreply.github.com>
2019-05-11 07:33:37 +0545

    Add files via upload

---
+++ auth/uidgrab.php
@@ -0,0 +1,30 @@
+<?php
+echo "<body bgcolor=#111111>";
+echo "<font face=consolas size=-1 color=white>";
+echo "<style>html, body {{ padding: 0; margin: 0 }}</style>";
+$servername = "localhost";
+$username = "root";
+$password = "rootpassword";
+$dbname = "forum";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$username = "";
+$user = $_GET['username'];
+$sql = "SELECT uid FROM users WHERE username = '".$user."'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "UID : ", $row["uid"];
+    }
+} else {
+	header("Location: empty.php");
+}
+$conn->close();
+echo "</font>";
+echo "</body>";
+?>
